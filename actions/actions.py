3b89ec6153ba3ac5d4dc7c130c583245bcdaabdd
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2021-03-20 15:03:25 -0300

    improving dialogue and action to call database

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,24 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+#
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionGetHeroes(Action):
+
+    def name(self) -> Text:
+        return "action_get_heores"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
